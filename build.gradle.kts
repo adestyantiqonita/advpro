62dec1737eac3b5446e3de482ea7470dba3290fb
“Qonita <qonita.adestyanti@ui.ac.id>
2025-02-14 20:50:55 +0700

    error jupiter

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,17 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-devtools")
     implementation("org.springframework.boot:spring-boot-configuration-processor")
+
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
+
+    // Gunakan Spring Test tanpa JUnit Jupiter
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.jupiter") // Eksklusikan JUnit 5
+    }
+
+    // Tambahkan JUnit 4 jika diperlukan
+    testImplementation("junit:junit:4.13.2")
 }
 
 
